ba9bf0d0f5fc619d6a3f6ac0be389a8758d5d87b
AksaMJML <akaks0186@gmail.com>
2026-04-24 21:39:42 +0530

    Add homepage carousel and album section to home.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,14 @@
 let navBar = document.getElementById("navBar")
+let root = document.getElementById("root")
 
 fetch("components/navBar/navbar.html")
     .then(response => response.text())
     .then(data => {
         navBar.innerHTML += data;
+    })
+
+fetch("components/home/home.html")
+    .then(response => response.text())
+    .then(data => {
+        root.innerHTML += data;
     })
