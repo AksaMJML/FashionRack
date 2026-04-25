de3313bba336a3caa1dcf90782eaa72674b8e371
AksaMJML <akaks0186@gmail.com>
2026-04-25 19:59:35 +0530

    Refactor code structure for improved readability and maintainability

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,88 @@ fetch("components/home/home.html")
     .then(data => {
         root.innerHTML += data;
     })
+
+
+let inventory = [
+  {
+    id: 1,
+    name: "Classic Black Blouse",
+    price: 24.99,
+    size: "M",
+    color: "Black",
+    category: "TOPS",
+    image: "images/1.jpg" 
+  },
+  {
+    id: 2,
+    name: "Khaki Casual Blazer",
+    price: 45.00,
+    size: "L",
+    color: "Khaki",
+    category: "BLAZERS",
+    image: "images/2.jpg"
+  },
+  {
+    id: 3,
+    name: "Beige Button-Down Dress",
+    price: 35.50,
+    size: "S",
+    color: "Beige",
+    category: "DRESSES",
+    image: "images/3.jpg"
+  },
+  {
+    id: 4,
+    name: "Light Blue Office Shirt",
+    price: 29.99,
+    size: "M",
+    color: "Blue",
+    category: "SHIRTS",
+    image: "images/4.jpg"
+  },
+  {
+    id: 5,
+    name: "Dusty Pink Basic Top",
+    price: 15.99,
+    size: "M",
+    color: "Pink",
+    category: "TOPS",
+    image: "images/5.jpg"
+  },
+  {
+    id: 6,
+    name: "Navy Blue Pleated Skirt",
+    price: 32.00,
+    size: "S",
+    color: "Navy",
+    category: "SKIRTS",
+    image: "images/6.jpg"
+  },
+  {
+    id: 7,
+    name: "Floral Midi Skirt",
+    price: 28.50,
+    size: "L",
+    color: "Floral",
+    category: "SKIRTS",
+    image: "images/7.jpg"
+  },
+  {
+    id: 8,
+    name: "Brown Polo Shirt",
+    price: 19.99,
+    size: "M",
+    color: "Brown",
+    category: "SHIRTS",
+    image: "images/brown.jpg"
+  },
+  {
+    id: 9,
+    name: "Essential White T-Shirt",
+    price: 12.99,
+    size: "S",
+    color: "White",
+    category: "TOPS",
+    image: "images/white.jpg"
+  }
+];
