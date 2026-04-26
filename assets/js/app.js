ef4baa7ad14136d4bbcc51c92a960efbb472c16d
AksaMJML <akaks0186@gmail.com>
2026-04-26 15:29:57 +0530

    Add product listing functionality and style updates for product cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,93 @@ fetch("components/home/home.html")
     .then(response => response.text())
     .then(data => {
         root.innerHTML += data;
+        loadProducts();
     })
+
+
+let inventory = [
+  {
+    id: 1,
+    name: "Classic Black Blouse",
+    price: 24.99,
+    size: "M",
+    color: "Black",
+    category: "TOPS",
+    image: "assets/img/1.jpg" 
+  },
+  {
+    id: 2,
+    name: "Khaki Casual Blazer",
+    price: 45.00,
+    size: "L",
+    color: "Khaki",
+    category: "BLAZERS",
+    image: "assets/img/2.jpg"
+  },
+  {
+    id: 3,
+    name: "Beige Button-Down Dress",
+    price: 35.50,
+    size: "S",
+    color: "Beige",
+    category: "DRESSES",
+    image: "assets/img/3.jpg"
+  },
+  {
+    id: 4,
+    name: "Light Blue Office Shirt",
+    price: 29.99,
+    size: "M",
+    color: "Blue",
+    category: "SHIRTS",
+    image: "assets/img/4.jpg"
+  },
+  {
+    id: 5,
+    name: "Dusty Pink Basic Top",
+    price: 15.99,
+    size: "M",
+    color: "Pink",
+    category: "TOPS",
+    image: "assets/img/5.jpg"
+  },
+  {
+    id: 6,
+    name: "Navy Blue Pleated Skirt",
+    price: 32.00,
+    size: "S",
+    color: "Navy",
+    category: "SKIRTS",
+    image: "assets/img/6.jpg"
+  },
+  {
+    id: 7,
+    name: "Floral Midi Skirt",
+    price: 28.50,
+    size: "L",
+    color: "Floral",
+    category: "SKIRTS",
+    image: "assets/img/7.jpg"
+  },
+  {
+    id: 8,
+    name: "Brown Polo Shirt",
+    price: 19.99,
+    size: "M",
+    color: "Brown",
+    category: "SHIRTS",
+    image: "assets/img/brown.jpg"
+  },
+  {
+    id: 9,
+    name: "Essential White T-Shirt",
+    price: 12.99,
+    size: "S",
+    color: "White",
+    category: "TOPS",
+    image: "assets/img/white.jpg"
+  }
+];
 
 
 function loadProducts(){
@@ -21,8 +107,8 @@ function loadProducts(){
     inventory.forEach(item => {
         htmlString += `
       <div class="col">
-        <div class="card shadow-sm">
-          <img src="${item.image}" class="card-img-top" alt="Product Image">
+        <div class="card shadow-sm product-card">
+          <img src="${item.image}" class="card-img-top product-img" alt="Product Image">
           <div class="card-body">
             <h5 class="card-title"> ${item.name} </h5>
             <p class="card-text">Price: $${item.price.toFixed(2)}</p>
@@ -39,86 +125,5 @@ function loadProducts(){
 
 loadProducts();
 
-let inventory = [
-  {
-    id: 1,
-    name: "Classic Black Blouse",
-    price: 24.99,
-    size: "M",
-    color: "Black",
-    category: "TOPS",
-    image: "images/1.jpg" 
-  },
-  {
-    id: 2,
-    name: "Khaki Casual Blazer",
-    price: 45.00,
-    size: "L",
-    color: "Khaki",
-    category: "BLAZERS",
-    image: "images/2.jpg"
-  },
-  {
-    id: 3,
-    name: "Beige Button-Down Dress",
-    price: 35.50,
-    size: "S",
-    color: "Beige",
-    category: "DRESSES",
-    image: "images/3.jpg"
-  },
-  {
-    id: 4,
-    name: "Light Blue Office Shirt",
-    price: 29.99,
-    size: "M",
-    color: "Blue",
-    category: "SHIRTS",
-    image: "images/4.jpg"
-  },
-  {
-    id: 5,
-    name: "Dusty Pink Basic Top",
-    price: 15.99,
-    size: "M",
-    color: "Pink",
-    category: "TOPS",
-    image: "images/5.jpg"
-  },
-  {
-    id: 6,
-    name: "Navy Blue Pleated Skirt",
-    price: 32.00,
-    size: "S",
-    color: "Navy",
-    category: "SKIRTS",
-    image: "images/6.jpg"
-  },
-  {
-    id: 7,
-    name: "Floral Midi Skirt",
-    price: 28.50,
-    size: "L",
-    color: "Floral",
-    category: "SKIRTS",
-    image: "images/7.jpg"
-  },
-  {
-    id: 8,
-    name: "Brown Polo Shirt",
-    price: 19.99,
-    size: "M",
-    color: "Brown",
-    category: "SHIRTS",
-    image: "images/brown.jpg"
-  },
-  {
-    id: 9,
-    name: "Essential White T-Shirt",
-    price: 12.99,
-    size: "S",
-    color: "White",
-    category: "TOPS",
-    image: "images/white.jpg"
-  }
-];
+let cart = [ ];
+
